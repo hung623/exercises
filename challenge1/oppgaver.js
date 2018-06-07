6a8164c625bf7f4bfb25ec3e46a5b115fd0ca054
Arne Fismen <arnefismen@gmail.com>
2018-06-07 11:36:01 +0200

    feedback på oppgave 1

--- challenge1/oppgaver.js
+++ challenge1/oppgaver.js
@@ -3,9 +3,11 @@ console.log(latitude)
 
 var tall = '6'
 console.log(tall)
+// Dette er nå en streng, ikke et tall.
 
 var afrika = ['Chad', "Uganda"];
 console.log(afrika)
+// Du bør bruke en form for anførselstegn, jeg foretrekker ''
 
 var albertStats = {
   strenghtStat: "1",
@@ -13,4 +15,7 @@ var albertStats = {
   intelligenceStat: "160",
   status: "diseased"
 };
+
+// I moderne javascript så trenger du ikke bruke ; for å avslutte linjer, så det er like greit å slutte med det.
+
 console.log(albertStats)
