8cf1dedfa3211d9d87250123b8e7b6fc307ac732
hung623 <hung_van98@hotmail.com>
2018-06-07 11:32:46 +0200

    Oppgave 1 done

---
+++ challenge1/oppgaver.js
@@ -0,0 +1,16 @@
+var latitude = 'latitude'
+console.log(latitude)
+
+var tall = '6'
+console.log(tall)
+
+var afrika = ['Chad', "Uganda"];
+console.log(afrika)
+
+var albertStats = {
+  strenghtStat: "1",
+  dexterityStat: "0",
+  intelligenceStat: "160",
+  status: "diseased"
+};
+console.log(albertStats)
