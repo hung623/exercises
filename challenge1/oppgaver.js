bcba94ccd86eec2486f205a232a32493c8c54e27
Arne Fismen <arnefismen@gmail.com>
2018-06-07 12:32:06 +0200

    added some logic to oppgaver

--- challenge1/oppgaver.js
+++ challenge1/oppgaver.js
@@ -1,19 +1,23 @@
 var latitude = 'latitude'
 console.log(latitude)
 
-var tall = 6;
+var tall = 6
 console.log(tall)
 // Dette er nå en streng, ikke et tall.
 
-var afrika = ['Chad', 'Uganda'];
+var afrika = ['Chad', 'Uganda']
 console.log(afrika)
+console.log(afrika[0])
+for (var i = 0; i < afrika.length; i++) {
+  console.log(afrika[i])
+}
 // Du bør bruke en form for anførselstegn, jeg foretrekker ''
 
 var albertStats = {
   strenghtStat: 1,
   dexterityStat: 0,
   intelligenceStat: 160,
-  status: "diseased"
+  status: 'diseased'
 }
 
 // I moderne javascript så trenger du ikke bruke ; for å avslutte linjer, så det er like greit å slutte med det.
