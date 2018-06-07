d80a839848227892ce68a66c14e933f2c02be154
Arne Fismen <arnefismen@gmail.com>
2018-06-07 13:31:16 +0200

    added while loops to for.js

--- challenge1/for.js
+++ challenge1/for.js
@@ -5,3 +5,10 @@ for (var i = 10; i > -1; i--) {
 // for kjører koden et par ganger, så var i har vi satt variablen til i
 // i sin value er 10; og da hvis i ikke er mindre en -1; så kjører den 
 // i-- som tar minus på 10 helt til den er 0
+
+// En anne måte å gjøre løkker (repetere oppgaver), er å bruke while.
+let counter = 0
+while (counter < 10) {
+  console.log(counter)
+  counter++
+}
