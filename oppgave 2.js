74e2187eefec8776103ddfae272bb213eea15256
hung623 <hung_van98@hotmail.com>
2018-06-07 15:52:39 +0200

    Oppgave 2 (streng og tall)

--- oppgave 2.js
+++ oppgave 2.js
@@ -1,6 +1,4 @@
 function func(p, d) {
-  var navn = 'Hung'
-  console.log(navn, 'er ' + 20, 'år gammel')
   console.log(p + ' + ' + d + ' = ')
   console.log(p + d)
 
@@ -8,7 +6,14 @@ function func(p, d) {
 
 func(19, 1)
 
+function person(name, age) {
+  console.log(name + ' er ' + age + ' år gammel')
+}
 
+person('Hung', 20)
 
 // streng og tall skriver ut i konsollen et navn og en alder
 //  "Hung er 20 år"
+
+// vi setter value på name og age til Hung og 20 så printer vi 
+// det ut med console.log
