25a4f1ac8754e1271a00045eb343bd2d8268e292
hung623 <hung_van98@hotmail.com>
2018-06-07 11:38:56 +0200

    Endret på klammene rundt tall og text, gjort om strengen til et tall og fjernet ;.

--- challenge1/oppgaver.js
+++ challenge1/oppgaver.js
@@ -1,20 +1,20 @@
 var latitude = 'latitude'
 console.log(latitude)
 
-var tall = '6'
+var tall = 6;
 console.log(tall)
 // Dette er nå en streng, ikke et tall.
 
-var afrika = ['Chad', "Uganda"];
+var afrika = ['Chad', 'Uganda'];
 console.log(afrika)
 // Du bør bruke en form for anførselstegn, jeg foretrekker ''
 
 var albertStats = {
-  strenghtStat: "1",
-  dexterityStat: "0",
-  intelligenceStat: "160",
+  strenghtStat: 1,
+  dexterityStat: 0,
+  intelligenceStat: 160,
   status: "diseased"
-};
+}
 
 // I moderne javascript så trenger du ikke bruke ; for å avslutte linjer, så det er like greit å slutte med det.
 
