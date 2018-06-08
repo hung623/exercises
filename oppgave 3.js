b99cdb803edfbeb69cd6d3ae11f9a415e11c709e
Arne Fismen <arnefismen@gmail.com>
2018-06-08 13:30:03 +0200

    updated oppgaver

--- oppgave 3.js
+++ oppgave 3.js
@@ -1,4 +1,31 @@
-var euLand = ['Sweden', 'France', 'Germany', 'Russia', 'Denmark', ]
-for (var i = 0; i < euLand.length; i++) {
-  console.log(euLand[i])
-}
+var euLand = ['Sweden', 'France', 'Russia', 'Denmark', ]
+
+console.log('The below output is the original array')
+console.log(euLand)
+
+console.log('\nNow we use: euLand.push(\'Spain\')')
+euLand.push('Spain')
+console.log('This generates the following euLand variable:')
+console.log(euLand)
+
+console.log('\nNow we use: euLand.unshift(\'Turkey\')')
+euLand.unshift('Turkey')
+console.log('This generates the following euLand variable:')
+console.log(euLand)
+
+console.log('\nNow we use: euLand.shift(), this returns:')
+console.log(euLand.shift())
+console.log('And generates the following euLand variable:')
+console.log(euLand)
+
+console.log('\nNow we use: euLand.pop(), this returns:')
+console.log(euLand.pop())
+console.log('And generates the following euLand variable:')
+console.log(euLand)
+
+
+// console.log(euLand.length)
+
+// for (var i = 0; i < euLand.length; i++) {
+//   console.log(euLand[i])
+// }
