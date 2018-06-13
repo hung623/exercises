ab5895a1fb6b14ee560b1db327edc788a95e8764
hung623 <hung_van98@hotmail.com>
2018-06-13 10:00:19 +0200

    lagt til spec oppgaven

---
+++ MAR/main.js
@@ -0,0 +1,32 @@
+{
+  "iktEmployes";
+  [{
+      "name": "Audun",
+      "lastName": "Ellertsen"
+    },
+    {
+      "name": "Arne",
+      "lastName": "Fismen"
+    },
+    {
+      "name": "Hung",
+      "lastName": "Van Tran"
+    },
+    {
+      "name": "Jan",
+      "lastName": "Henning Thorsen"
+    },
+    {
+      "name": "Nicolas",
+      "lastName": "Mendoza"
+    },
+    {
+      "name": "Marcus",
+      "lastName": "Ramberg"
+    },
+    {
+      "name": "Rune",
+      "lastName": "Sandbakken"
+    }
+  ]
+}
